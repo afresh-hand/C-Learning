c499511d3954d1420888255e3148f38f2b7003f0
RookieProgrammer <291505205@qq.com>
2022-12-16 08:45:36 +0800

    initalize the reposity

---
+++ src/first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // float a = 50.25, b = 11.17;
+    // cout.setf(ios_base::fixed, ios_base::floatfield);
+    // cout << a + b << endl;
+    int a = {4.5};
+    cout << "the value of a is " << a <<endl;
+    
+    
+}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = {4.5};
+    cout << "the value of a is " <<a<< endl;
+}
