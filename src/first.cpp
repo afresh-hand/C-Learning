a602b8836c21ed7ce76aa84cc929901df42fe60c
HongbinGao <291505205@qq.com>
2022-12-17 17:37:38 +0800

    update the note for chapter 4

--- src/first.cpp
+++ src/first.cpp
@@ -8,11 +8,13 @@ int main()
     // float a = 50.25, b = 11.17;
     // cout.setf(ios_base::fixed, ios_base::floatfield);
     // cout << a + b << endl;
-    string a;
-    char b[9];
-    cin.getline(b, 9);
-
-    cout << "the value of a is " << b <<endl;
+    char a[] = "Felonia Ripova";
+    char32_t b[] = U"Humber SUper Snipe";
+    // string a= "Felonia Ripova";
+    // string b = "Humber SUper Snipe";
+    cout<< "example for char16 " << a << endl;
+    cout<< "example for char32 " << b << endl;
     
+    return 0;
     
 }
