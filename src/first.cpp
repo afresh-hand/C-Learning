228ac210c7c4d51b5063f6ea75bd37e29f068575
RookieProgrammer <291505205@qq.com>
2022-12-17 13:56:26 +0800

    update the note for chapter 4

--- src/first.cpp
+++ src/first.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
 int main()
@@ -6,8 +8,11 @@ int main()
     // float a = 50.25, b = 11.17;
     // cout.setf(ios_base::fixed, ios_base::floatfield);
     // cout << a + b << endl;
-    int a = {4.5};
-    cout << "the value of a is " << a <<endl;
+    string a;
+    char b[9];
+    cin.getline(b, 9);
+
+    cout << "the value of a is " << b <<endl;
     
     
 }
